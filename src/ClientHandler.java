d4c346d088612303f75fa5f13bafd4e40e3d346b
Rafael Borges <rafaborgesdasilva75@gmail.com>
2026-01-01 01:12:57 -0300

    minor changes: added line for creating the thread pool

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -1,11 +1,17 @@
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ClientHandler implements Runnable {
     private Socket socket;
 
+    // creating the thread pool
+    ExecutorService service = Executors.newFixedThreadPool(1000);
+
     void Handler(Socket socket) {
         this.socket = socket;
     }
+
     public ClientHandler(Socket socket) {
         this.socket = socket;
     }
