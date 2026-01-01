a3cb063cf8adbafcf58062d95064657d2eae1715
Rafael <rafaborgesdasilva75@gmail.com>
2026-01-01 16:04:59 -0300

    Fixed server code

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,19 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         int port = 8080;
 
         try (ServerSocket server = new ServerSocket(port)) {
             ExecutorService pool = Executors.newFixedThreadPool(1000);
             System.out.println("> Servidor Iniciado na porta: " + port);
+
+            while (true) {
+                Socket clientSocket = server.accept();
+                System.out.println("> Cliente conectado: " + clientSocket.getInetAddress().getHostAddress());
+                pool.execute(new ClientHandler(clientSocket));
+            }
 
         } catch (IOException err){
             System.out.println("> Erro ao iniciar o servidor: " + err.getMessage());
