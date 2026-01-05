5bc104d3a44be02d81eaa89e8d3284c4d19ae495
Rafael <rafaborgesdasilva75@gmail.com>
2026-01-05 17:40:37 -0300

    added comments for better understanding of the code

--- src/GetCommand.java
+++ src/GetCommand.java
@@ -4,12 +4,17 @@ public class GetCommand implements Command {
 
     @Override
     public void execute(DataStore store, String[] args, PrintStream out) {
+
+        // verifica se o numero de argumentos está correto
+        // espera que o args[0] seja "GET", args[1] seja a chave
         if (args.length < 2) {
             out.println(">> ERRO: Uso incorreto do comando GET. Uso: GET <chave>");
             return;
         }
+
         String key = args[1];
         String value = store.get(key);
+
         if (value != null) {
             out.println(">> COMANDO GET ACEITO: " + key + " -> " + value);
         } else {
