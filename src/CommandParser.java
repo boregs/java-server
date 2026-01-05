8be189a15d3a18565c3298a51132ff599af42f53
Rafael <rafaborgesdasilva75@gmail.com>
2026-01-05 17:46:53 -0300

    class to contain the commands of the user in an organized way

--- src/CommandParser.java
+++ src/CommandParser.java
@@ -1,2 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class CommandParser {
+    private static final Map<String, Command> command = new HashMap<>();
+
+    static {
+        command.put("SET", new SetCommand());
+        command.put("GET", new GetCommand());
+    }
+
+    public static Command parser(String commandNamed){
+        return command.getOrDefault(commandNamed.toUpperCase(), new UnknownCommand());
+    }
 }
