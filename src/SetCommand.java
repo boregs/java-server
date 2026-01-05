3b24d8b9392d4cb34f9e2bff9e8a531801bd85f8
Rafael <rafaborgesdasilva75@gmail.com>
2026-01-05 17:40:17 -0300

    added comments for better understanding of the code

--- src/SetCommand.java
+++ src/SetCommand.java
@@ -3,6 +3,9 @@ import java.io.PrintStream;
 public class SetCommand implements Command{
     @Override
     public void execute(DataStore store, String[] args, PrintStream out){
+
+        // verifica se o numero de argumentos está correto
+        // espera que o args[0] seja "SET", args[1] seja a chave e args[2] seja o valor
         if (args.length < 3) {
             out.println(">> ERRO: Uso incorreto do comando SET. Uso: SET <chave> <valor>");
             return;
