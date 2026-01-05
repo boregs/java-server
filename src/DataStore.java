2dd24b1ee47aaadd927b9f6f0e885fbfb39e9404
Rafael <rafaborgesdasilva75@gmail.com>
2026-01-04 22:00:27 -0300

    Updated file to incluide put, get and size methods for the hashmap

--- src/DataStore.java
+++ src/DataStore.java
@@ -1,5 +1,42 @@
 import java.util.Map;
+import java.util.HashMap;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.locks.Lock;
 
 public class DataStore {
-    Map<String, String> storage;
+    private final Map<String, String> storage = new HashMap<>(); // hashmap para guardar os usuarios conectados
+
+    private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+    private final Lock readLock = lock.readLock();
+    private final Lock writeLock = lock.writeLock();
+
+    // METODO PARA COLOCAR ITENS NO MAPA DE FORMA THREAD-SAFE
+    public void put(String key, String value){
+        writeLock.lock();
+        try {
+            storage.put(key, value);
+        } finally {
+            writeLock.unlock();
+        }
+    }
+
+    // METODO PARA PEGAR ITENS DO MAPA DE FORMA THREAD-SAFE
+    public String get(String key){
+        readLock.lock();
+        try {
+            return storage.get(key);
+        } finally {
+            readLock.unlock();
+        }
+    }
+
+    //METODO PARA VERIFICAR O TAMANHO DO MAPA
+    public int size() {
+        readLock.lock();
+        try {
+            return storage.size();
+        } finally {
+            readLock.unlock();
+        }
+    }
 }
