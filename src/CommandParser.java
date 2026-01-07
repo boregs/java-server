bc3d9e8663cc4a774a10f684000a9ac2a44a56ed
Rafael <rafaborgesdasilva75@gmail.com>
2026-01-06 21:06:27 -0300

    minor changes: comments for better understanding

--- src/CommandParser.java
+++ src/CommandParser.java
@@ -4,11 +4,13 @@ import java.util.Map;
 public class CommandParser {
     private static final Map<String, Command> command = new HashMap<>();
 
+    // inicializa o mapa de comandos
     static {
         command.put("SET", new SetCommand());
         command.put("GET", new GetCommand());
     }
 
+    // retorna o comando correspondente ao nome, ou UnknownCommand se nao existir
     public static Command parser(String commandNamed){
         return command.getOrDefault(commandNamed.toUpperCase(), new UnknownCommand());
     }
